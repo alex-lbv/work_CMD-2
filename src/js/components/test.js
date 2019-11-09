10fe53cd7220d75bb0506bf43be2dca64e0db641
Mac <mac@MacBook-Air-Macbook-5.local>
2019-11-10 00:07:18 +0300

    feature: adds

--- src/js/components/test.js
+++ src/js/components/test.js
@@ -1,16 +1,32 @@
 var testPoints = 5;
 
+function num2str(n, text_forms) {
+  n = Math.abs(n) % 100;
+  var n1 = n % 10;
+  if (n > 10 && n < 20) {
+    return text_forms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return text_forms[1];
+  }
+  if (n1 == 1) {
+    return text_forms[0];
+  }
+  return text_forms[2];
+}
+
 function decrementPoints() {
   --testPoints;
 }
 
 function testResult() {
-  if (testPoints >= 0 && testPoints <= 3) {
+  if (testPoints >= 0 && testPoints < 3) {
     $('#res-3').removeClass('uk-hidden');
-  } else if (testPoints > 3 && testPoints <= 4) {
+  } else if (testPoints >= 3 && testPoints <= 4) {
     $('#res-2').removeClass('uk-hidden');
   } else if (testPoints == 5) {
     $('#res-1').removeClass('uk-hidden');
   }
   $('.your-points').html(testPoints);
+  $('.your-point-word').html(num2str(testPoints, ['балл', 'балла', 'баллов']));
 }
